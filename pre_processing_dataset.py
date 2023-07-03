40fa9f63054a5df6b8b511deb86a012c48691b33
khanarslaan7861 <khanarslaan7861@gmail.com>
2023-07-03 21:25:24 +0530

    modified:   pre_processing_dataset.py

--- pre_processing_dataset.py
+++ pre_processing_dataset.py
@@ -49,4 +49,39 @@ for i in port_dicts:
     if port_dicts[i] == max_used_port_value:
         print(f"Most used port is '{i}' with '{max_used_port_value}' calls")
         max_used_port = i
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+print('             Attacked and Not attacked values for most used port')
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+# Attacked and Not attacked values for most used port
+print(f"Port {max_used_port}, used: {len(df.loc[(df[' Destination Port'] == 80)])}")
+print(f"Port {max_used_port}, attacked: {len(df.loc[(df[' Destination Port'] == 80) & (df[' Label'] == 1)])}")
+print(f"Port {max_used_port}, not attacked: {len(df.loc[(df[' Destination Port'] == 80) & (df[' Label'] == 0)])}")
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+# Optimize int type values
+for i in columns:
+    if df[f"{i}"].dtype == 'int64':
+        if df[f"{i}"].max() < 127:
+            df[f"{i}"] = df[f"{i}"].astype('int8')
+        elif df[f"{i}"].max() < 32767:
+            df[f"{i}"] = df[f"{i}"].astype('int16')
+        elif df[f"{i}"].max() < 2 * 10 ** 9:
+            df[f"{i}"] = df[f"{i}"].astype('int32')
+    else:
+        continue
+print('                                 Null values in columns')
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
 
+# Visualize how many null values exist in data in descending
+print(df.isnull().sum(axis=0).sort_values(ascending=False))
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+print('                                 After dropping null values')
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+# Drop null values(samples) and replace
+df.dropna(subset=['Flow Bytes/s'], inplace=True)
+print(df.isnull().sum(axis=0).sort_values(ascending=False))
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+print('                                       DF Describe')
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
+# pd.set_option('display.max_columns', None)
+print(df.describe())
+print('--------x--------x--------x--------x--------x--------x--------x--------x--------x--------')
