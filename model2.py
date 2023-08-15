6da0c43cd95129941aa187313db5087e354ac6b6
khanarslaan7861 <khanarslaan7861@gmail.com>
2023-08-15 17:31:56 +0530

    model2.py: Optimized imports

--- model2.py
+++ model2.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score, cross_val_predict
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_predict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, roc_auc_score, \
     confusion_matrix, ConfusionMatrixDisplay, RocCurveDisplay
 from sklearn.linear_model import LogisticRegression
