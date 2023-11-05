b1635c2181dc6721cd9b8c9b055ae0d125b1a7b1
khanarslaan7861 <khanarslaan7861@gmail.com>
2023-11-06 02:15:27 +0530

    Made 100 score predictions for score.pkl 	modified:   src/test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 import pickle
 import os
+
 cd = os.path.dirname(__file__) + '/../pkl/scores.pkl'
 
 with open(cd, 'rb') as file:
-        pkl = pickle.load(file)
-        print(pkl)
+    pkl = pickle.load(file)
+    print(pkl)
